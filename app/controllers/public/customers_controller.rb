e8a033114f831bb985dc7ac446f351d29372bbea
maronny <133176932+maronyy@users.noreply.github.com>
2024-05-24 18:55:28 +0000

    [Add]customer編集機能

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,9 +4,13 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to customer_path(@customer.id)
   end
   
   def unsubscribe
@@ -14,6 +18,11 @@ class Public::CustomersController < ApplicationController
     @customer.update(is_active: false)
     flash[:notice] = "ご利用ありがとうございました。"
     redirect_to root_path
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :adress, :telephone_number, :is_active)
   end
 
 end
