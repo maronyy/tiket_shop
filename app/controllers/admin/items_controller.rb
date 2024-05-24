3edcf929b9ceb1d2893d19bf3f0a3dda2808acf5
maronny <133176932+maronyy@users.noreply.github.com>
2024-05-24 16:33:58 +0000

    [Add]itemの機能全般

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -18,9 +18,23 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
-  def edit
+ def edit
+    @item = Item.find(params[:id])
+ end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to admin_items_path
   end
 
   private
